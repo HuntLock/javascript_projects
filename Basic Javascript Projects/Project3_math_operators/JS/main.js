d541f034946e078c7ded82d6167474bbf7ea0e1a
HuntLock <hunter.lock02@gmail.com>
2021-04-12 14:24:35 -0600

    Math
    Assigned basic math functions

--- Basic Javascript Projects/Project3_math_operators/JS/main.js
+++ Basic Javascript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,23 @@
-function addition_Function() {
-    var addition= 2+2;
-    document.getElementById ("Math") .innerHTML= "2+2=" +addition;
+function multiplication_function() {
+    var multiplication=6*8;
+    document.getElementById("Math").innerHTML= "6 x 8 =" +multiplication;
 }
+multiplication_function();
+
+function subtraction_Function() {
+    var subtraction= 5-2;
+    document.getElementById("Math").innerHTML= "5-2=" +subtraction;
+}
+Subtraction_Function();
+
+function addition_Function() {
+    var addition= 5+3;
+    document.getElementById("Math").innerHTML= "5+3=" +addition;
+}
+addition_Function();
+
+function division_Function() {
+    var division= 48/6;
+    document.getElementById("Math").innerHTML= "48/6=" +division;
+}
+division_Function();
