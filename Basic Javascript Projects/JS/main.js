f1485ed9fb0c3e89d6e7959858ecb4614222d8ea
HuntLock <hunter.lock02@gmail.com>
2021-04-12 10:43:18 -0600

    JS Update

---
+++ Basic Javascript Projects/JS/main.js
@@ -0,0 +1,18 @@
+function My_First_Function(){ //this is a function
+    var str="This is a string";
+    var result=str.fontcolor("pink");
+    document.getElementById("Pink_Text").innerHTML=result;
+} 
+
+windows.alert("Hello, world!"); //this is an alert
+
+var X= "Hello World"; //variable 1
+
+var Y= "Hello World" + "Hello all"; //variable 2
+
+var blues= "I have the blues."; // Button
+var blues= blues.fontcolor ("blue");
+document.write (blues);
+
+Document.write(3 + 3); //Here is an expression
+
