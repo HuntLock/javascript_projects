75c8b798d022dfe88f07b4d497146aac95b2013b
HuntLock <hunter.lock02@gmail.com>
2021-04-12 16:45:49 -0600

    Update

--- Basic Javascript Projects/Project3_math_operators/JS/main.js
+++ Basic Javascript Projects/Project3_math_operators/JS/main.js
@@ -1,6 +1,6 @@
 function multiplication_function() {
     var multiplication=6*8;
-    document.getElementById("Math").innerHTML= "6 x 8 =" +multiplication;
+    document.getElementById("Math").innerHTML= "6*8 =" +multiplication;
 }
 multiplication_function();
 
@@ -21,3 +21,40 @@ function division_Function() {
     document.getElementById("Math").innerHTML= "48/6=" +division;
 }
 division_Function();
+
+function more_function() {
+    var more= (1+2)*10/2-5;
+    document.getElementById("Math").innerHTML= "(1+2)*10/2-5=" +more;
+}
+more_function();
+
+function modulus_operator() {
+    var simple= 25 % 6;
+    document.getElementById("Math").innerHTML= "25 % 6=" +simple
+} 
+modulus_operator();
+
+function negation_Operator(){
+    var X=10;
+    document.getElementById("Math").innerHTML= -X;
+}
+negation_Operator;
+
+function increment(){
+    var X=5;
+    X++;
+    document.write(X);
+}
+function_increment();
+
+function decrement(){
+    var X= 5.25;
+    X--;
+    document.write(X);
+}
+function_decrement();
+
+function alert(){
+window.alert(math.random());
+}
+function_alert();
