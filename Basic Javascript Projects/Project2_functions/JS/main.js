7999e091522a6115d39e95de16c89e1b21e9587d
HuntLock <hunter.lock02@gmail.com>
2021-04-12 11:10:27 -0600

    Function
    Built a function per the assignment

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function My_First_Function(){ //this is a function
+    var str="This is a string";
+    var result=str.fontcolor("pink");
+    document.getElementById("Pink_Text").innerHTML=result;
+} 
