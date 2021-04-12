406618e33e0cc63750e4e33fd0c544b10038423f
HuntLock <hunter.lock02@gmail.com>
2021-04-12 12:08:58 -0600

    Function update

--- Basic Javascript Projects/Project2_functions/JS/main.js
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -3,3 +3,9 @@ function My_First_Function(){ //this is a function
     var result=str.fontcolor("pink");
     document.getElementById("Pink_Text").innerHTML=result;
 } 
+
+function My_Function(){ //this is a function with a concatenate
+    var sentence="I am learning";
+    sentence+= " a lot from this book";
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
