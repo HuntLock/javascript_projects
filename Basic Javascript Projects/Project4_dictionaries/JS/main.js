251e87c042b992923ebafeb5247595372e7b3ef5
HuntLock <hunter.lock02@gmail.com>
2021-04-15 13:24:05 -0600

    Delete

--- Basic Javascript Projects/Project4_dictionaries/JS/main.js
+++ Basic Javascript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,11 @@
 function my_Dictionary(){
-    var Animal={
+    var Animal={//Set Key Value Pairs below
         Species: "Platypus",
         Color: "Bluish-Green",
         Breed: "Secret Agent",
         Age: "6",
-        Sound:"gnarrg.g.g.g",
+        Sound:"gnarrg.g.g.g", 
     };
-    document.getElementById("Dictionary").innerHTML=Animal.Sound;
+    delete Animal.Sound; //Remove the Key: Sound
+    document.getElementById("Dictionary").innerHTML=Animal.Sound; //Display Sound. Unable to do so since it has been removed
 }
