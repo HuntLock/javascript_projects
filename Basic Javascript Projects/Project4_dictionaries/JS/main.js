a9cd735fa8fefe3dd93037cf2c40a2ac1686e3f4
HuntLock <hunter.lock02@gmail.com>
2021-04-15 13:18:11 -0600

    Dictionary: Animal noise

---
+++ Basic Javascript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function my_Dictionary(){
+    var Animal={
+        Species: "Platypus",
+        Color: "Bluish-Green",
+        Breed: "Secret Agent",
+        Age: "6",
+        Sound:"gnarrg.g.g.g",
+    };
+    document.getElementById("Dictionary").innerHTML=Animal.Sound;
+}
