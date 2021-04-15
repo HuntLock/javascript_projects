efd77fd36c8b6887b209f3eb80983d9dfb487f73
HuntLock <hunter.lock02@gmail.com>
2021-04-15 12:52:49 -0600

    Repair

--- Basic Javascript Projects/Project3_math_operators/JS/main.js
+++ Basic Javascript Projects/Project3_math_operators/JS/main.js
@@ -1,60 +1,51 @@
-function multiplication_function() { //This is a Multiply
+function multiplication_function() { //This is a multiplication
     var multiplication=6*8;
     document.getElementById("Math").innerHTML= "6*8 =" +multiplication;
 }
-multiplication_function();
 
 function subtraction_Function() { //This is a subtraction
     var subtraction= 5-2;
     document.getElementById("Math").innerHTML= "5-2=" +subtraction;
 }
-Subtraction_Function();
 
 function addition_Function() { //This is an addition
     var addition= 5+3;
     document.getElementById("Math").innerHTML= "5+3=" +addition;
 }
-addition_Function();
 
 function division_Function() { //This is a division
     var division= 48/6;
     document.getElementById("Math").innerHTML= "48/6=" +division;
 }
-division_Function();
 
 function more_function() { //This is a multi-step equation
     var more= (1+2)*10/2-5;
     document.getElementById("Math").innerHTML= "(1+2)*10/2-5=" +more;
 }
-more_function();
 
 function modulus_operator() { //This is a Modulus operator
     var simple= 25 % 6;
     document.getElementById("Math").innerHTML= "25 % 6=" +simple
 } 
-modulus_operator();
 
 function negation_Operator(){ //This is a negation
     var X=10;
     document.getElementById("Math").innerHTML= -X;
 }
-negation_Operator;
 
-function increment(){ //This is an increment
+function increment_Operator(){ //This is a negation
     var X=5;
     X++;
-    document.write(X);
+    document.getElementById("Math").innerHTML= X;
+
 }
-function_increment();
 
 function decrement(){ //This is a decrement
     var X= 5.25;
     X--;
     document.write(X);
 }
-function_decrement();
 
-function alert(){ //This is an alert
-window.alert(math.random());
+function alertlert(){ //This is an alert
+window.alert("Hello");
 }
-function_alert();
