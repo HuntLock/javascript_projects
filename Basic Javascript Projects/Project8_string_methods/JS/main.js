7cf69c952c984bba13c34b6e8c9688c2ac215134
HuntLock <hunter.lock02@gmail.com>
2021-04-17 12:17:48 -0600

    Update main.js

--- Basic Javascript Projects/Project8_string_methods/JS/main.js
+++ Basic Javascript Projects/Project8_string_methods/JS/main.js
@@ -1,24 +1,24 @@
-function full_sentence(){
-    var part_1= "I have ";
-    var part_2= "made this ";
-    var part_3= "into a ";
-    var part_4= "complete sentence.";
+function full_sentence(){ //full sentence
+    var part_1= "I have "; //p 1
+    var part_2= "made this "; //p 2
+    var part_3= "into a "; //p 3
+    var part_4= "complete sentence."; //p 4
     var whole_sentence= part_1.concat(part_2, part_3, part_4);
     document.getElementById("Concatenate").innerHTML=whole_sentence;
 }
 
-function slice_method(){
+function slice_method(){ //slice method
     var sentence= "All work and no play makes johnny a dull boy.";
     var section= sentence.slice(27,33);
     document.getElementById("slice").innerHTML=section;
 }
 
-function String_method(){
+function String_method(){ //string method
     var x=182;
-    document.getElementById("numbers_to_string").innerHTML=x.toString();
+    document.getElementById("numbers_to_string").innerHTML=x.toString(); 
 }
 
-function precision_method(){
+function precision_method(){ //precision method
     var x= 12938.3012987376112;
     document.getElementById("Precision").innerHTML=x.toPrecision(10);
 }
