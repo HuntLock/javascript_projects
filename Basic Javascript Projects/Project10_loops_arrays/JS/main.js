8758c89da2b22e8a027907ab88fadee7994737b0
HuntLock <hunter.lock02@gmail.com>
2021-04-19 13:23:46 -0600

    Update

--- Basic Javascript Projects/Project10_loops_arrays/JS/main.js
+++ Basic Javascript Projects/Project10_loops_arrays/JS/main.js
@@ -1,3 +1,4 @@
+//While Loop
 function count_to_ten(){
     var Digit="";
     var x=1;
@@ -8,6 +9,7 @@ function count_to_ten(){
     document.getElementById("counting_to_ten").innerHTML= Digit;
 }
 
+//For Loop
 var Instruments= ["Guitar", "Drums", "Bass", "Keyboard", "Trumpet", "Ukulele"];
 var Content= "";
 var Y;
@@ -18,6 +20,7 @@ function for_Loop(){
     document.getElementById("List_of_instruments").innerHTML= Content;
 }
 
+//Array
 function cat_pics(){
     var Cat_Picture= [];
     Cat_Picture[0]="Sleeping";
@@ -28,3 +31,34 @@ function cat_pics(){
     document.getElementById("Cat").innerHTML="In this picture, the cat is "+
         Cat_Picture[2]+ ".";
 }
+//CONSTANT
+function constant_function(){
+    const Musical_instrument= {type:"guitar", brand:"fender", color:"black"};
+    Musical_instrument.color="blue";
+    Musical_instrument.price="$900";
+    document.getElementById("Constant").innerHTML="The cost of the " +
+        Musical_instrument.type + " was " + Musical_instrument.price;
+}
+
+//lET Example 1
+var Y=92; 
+document.write(Y);
+{
+    let Y=13;
+    document.write ("<br>"+Y);
+}
+document.write("<br>" + Y);
+
+//LET expanded
+
+let Batman={
+    identity: "Bruce Wayne",
+    skill: "detective",
+    weapon: "batarang",
+    color: "black",
+    description: function(){
+        return "The batman is " + this.identity + " He is a(n) "+ this.skill + " he use a(n) " + this.weapon + " his suit is " + this.color;
+    }
+}
+document.getElementById("Batman_Object").innerHTML= Batman.description
+
